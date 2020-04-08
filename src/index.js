34f6259060976295d9d90c3a5b60f0bbd28d21a0
Adrien.Y <adrien.ybanez@hotmail.fr>
2020-04-08 16:58:23 +0200

    Récupération des articles depuis la base et affichage de ces derniers + ajout de la méthode pour supprimer un article

--- src/index.js
+++ src/index.js
@@ -1,2 +1,68 @@
 import './assets/styles/styles.scss';
 import './index.scss';
+
+const articleContainerElem = document.querySelector('.articles-container');
+
+
+/*
+    Fonction récupérant tous les articles dans la base de données
+*/
+const fetchArticles = async () => {
+    try {
+        const response = await fetch("https:restapi.fr/api/blogs");
+        const bodyResponse = await response.json();
+        console.log('mes articles : ', bodyResponse)
+        createArticles(bodyResponse);
+    } catch(e) {
+        console.log('erreur : ', e);
+    }
+}
+
+
+/*
+    Fonction créant les éléments html à partir de la liste 
+    des articles reçu en paramètre
+    Ajout des event listener sur les btn
+*/
+const createArticles = (listeArticles) => {
+    const articlesDOM = listeArticles.map( (article) => {
+        const articleDOM = document.createElement('div');
+        articleDOM.classList.add('article');
+        articleDOM.innerHTML = `
+        <img src="${ article.image }" alt="Image de profil">
+        <h2>${ article.title }</h2>
+        <p class="article-author">${ article.author - article.categorie }</p>
+        <p class="article-content">
+            ${ article.content }
+        </p>
+        <div class="article-actions">
+            <button class="btn btn-suppression" data-id=${ article._id }>Supprimer</button>
+            <button class="btn btn-primary" data-id=${ article._id }>Modifier</button>
+        </div>
+        `
+        return articleDOM
+    });
+    console.log('mes articles DOM : ', articlesDOM);
+    articleContainerElem.innerHTML = '';
+    articleContainerElem.append(...articlesDOM);
+    const deleteButtons = articleContainerElem.querySelectorAll('.btn-suppression');
+    deleteButtons.forEach( button => {
+        button.addEventListener('click', async e => {
+            try { 
+                const target = e.target;
+                const articleId = target.dataset.id;
+                const response = await fetch(`https://restapi.fr/api/blogs/${ articleId }`, {
+                    method: "DELETE"
+                });
+                const body = await response.json();
+                console.log('mon retour : ', body);
+                fetchArticles();
+            } catch(e) {
+                console.log('erreur : ', e);
+            }
+
+        })
+    })
+}
+
+fetchArticles();
